cbde16655ebda8de1c84fd97bf5f440bef375780
Vasanth K <77053192+Vasanth160@users.noreply.github.com>
2025-11-01 15:49:46 +0530

    Update DigitalMarketing.java

--- Java_Practice programs/src/InmakesInternship/DigitalMarketing.java
+++ Java_Practice programs/src/InmakesInternship/DigitalMarketing.java
@@ -9,5 +9,17 @@ public class DigitalMarketing extends Multimedia {
 	public void searchEngine(String search) {
 		System.out.println(search);
 
+		String name1="VasanthMoorthy";
+		char[] ce = name1.toCharArray();
+		int CharArray[]=new int[256];
+		for(char value:ce) 
+			CharArray[value]++;
+		
+		for(int i=0;i<CharArray.length;i++) {
+			if(CharArray[i]>1)
+				System.out.println((char)i+" "+CharArray[i]);
+
+
 	}
 }
+
